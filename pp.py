46b16c428e31eb646da467bffa167a090af982f3
周旭 <zhouxusoft@gmail.com>
2023-05-27 19:48:20 +0800

    ..

--- pp.py
+++ pp.py
@@ -1,8 +1,39 @@
 import requests
+import pymysql
 
-# keyword = input('搜索：')
-keyword = '中文'
-url = f'https://www.fonts.net.cn/font-search-result.html?q={keyword}'
+db = pymysql.connect(
+    host="127.0.0.1",
+    user="root",
+    password="123456",
+    charset="utf8mb4"
+)
+
+dbcursor = db.cursor()
+dbcursor.execute("CREATE DATABASE IF NOT EXISTS font")
+dbcursor.execute("USE font")
+
+dbcursor.execute("CREATE TABLE IF NOT EXISTS `fontdata` (\
+                    `id` int NOT NULL AUTO_INCREMENT,\
+                    `font_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,\
+                    `font_number` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,\
+                    `font_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,\
+                    `font_imgurl` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,\
+                    `font_download` int NULL DEFAULT NULL,\
+                    `font_viewnum` int NULL DEFAULT NULL,\
+                    PRIMARY KEY (`id`) USING BTREE\
+                    ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;")
+
+dbcursor.execute("CREATE TABLE IF NOT EXISTS `fonttype` (\
+                    `id` int NOT NULL AUTO_INCREMENT,\
+                    `type_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,\
+                    PRIMARY KEY (`id`) USING BTREE\
+                    ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;")
+
+
+
+keyword = input('搜索：')
+# url = f'https://www.fonts.net.cn/font-search-result.html?q={keyword}'
+url = 'https://www.fonts.net.cn/fonts-zh-1.html'
 response = requests.get(url)
 data = response.content.decode('utf-8', 'ignore')
 
@@ -10,7 +41,7 @@ data = data.split('<ul class="site_font_list">')[1]
 data = data.split('</ul>')[0]
 
 datas = data.split('</li>')
-print(datas[0])
+# print(datas[0])
 
 for i in datas:
     if i == '':
